0a2fd7103ba131d66241d57b475a863b9535674f
chucshary <shary-princess@hotmail.com>
2015-04-02 12:32:23 -0500

    Map_Nuuk

---
+++ NuUkAppMobile/app/src/main/java/com/example/nuuk/nuukappmobile/Map.java
@@ -0,0 +1,135 @@
+package com.example.nuuk.nuukappmobile;
+
+import android.app.Activity;
+import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.List;
+import java.util.Locale;
+
+
+public class Map extends ActionBarActivity {
+    private GoogleMap googleMap;
+    private CameraUpdate mcamera;
+    private Location location;
+    private double latitude=0;
+    private double longitude=0;
+    private LocationManager locationManager;
+    private Activity context;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+        createMapView();
+        addOnMapClickListener();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_map, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void createMapView() {
+        if (googleMap == null) {
+            googleMap = ((MapFragment) getFragmentManager().findFragmentById(
+                    R.id.map)).getMap();
+            if (googleMap != null) {
+                googleMap.setMyLocationEnabled(true);
+                try {
+                    locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+                    location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                    longitude = location.getLongitude();
+                    latitude = location.getLatitude();
+                    LatLng latLng=new LatLng(latitude,longitude);
+
+                    List<Address> addresses;
+                    Geocoder geocoder = new Geocoder(getBaseContext(), Locale.getDefault());
+                    addresses = geocoder.getFromLocation(latLng.latitude,latLng.longitude, 1);
+                    String city = addresses.get(0).getLocality();
+                    String state = addresses.get(0).getAdminArea();
+                    String region = addresses.get(0).getSubLocality();
+                    String street = addresses.get(0).getThoroughfare();
+                    String country = addresses.get(0).getCountryName();
+                    setMarker(latLng,city+" "+" "+state+" "+region+" "+street+" "+country, "Latitud: " + latLng.latitude+ " Longitud: " + latLng.longitude);
+                }
+                catch (Exception e)
+                {
+                }
+            }
+        }
+    }
+
+    private void setMarker(LatLng position, String titulo, String info) {
+        // Agregamos marcadores para indicar sitios de interéses.
+        googleMap.addMarker(new MarkerOptions()
+                .position(position)
+                .title(titulo)  //Agrega un titulo al marcador
+                .snippet(info)   //Agrega información detalle relacionada con el marcador
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET))); //Color del marcador
+
+        // mMap.addMarker(new MarkerOptions().position(new LatLng(latitude,longitude)).title("Yo aqui").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET)).snippet("App Nu'uk"));
+        mcamera= CameraUpdateFactory.newLatLngZoom((position), 14);
+        googleMap.animateCamera(mcamera);
+    }
+
+    public void addOnMapClickListener()
+    {
+        googleMap = ((MapFragment) getFragmentManager().findFragmentById(
+                R.id.map)).getMap();
+        googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                try {
+                    List<Address> addresses;
+                    Geocoder geocoder = new Geocoder(getBaseContext(), Locale.getDefault());
+                    addresses = geocoder.getFromLocation(latLng.latitude,latLng.longitude, 1);
+                    String city = addresses.get(0).getLocality();
+                    String state = addresses.get(0).getAdminArea();
+                    String region = addresses.get(0).getSubLocality();
+                    String street = addresses.get(0).getThoroughfare();
+                    String country = addresses.get(0).getCountryName();
+                    setMarker(latLng,city+" "+" "+state+" "+region+" "+street+" "+country, "Latitud: " + latLng.latitude+ " Longitud: " + latLng.longitude);
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+    }
+}
