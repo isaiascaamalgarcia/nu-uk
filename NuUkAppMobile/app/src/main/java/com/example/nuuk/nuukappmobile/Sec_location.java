5c4c2278a0360386b1fde2989fca2c03c25374ab
chucshary <shary-princess@hotmail.com>
2015-04-07 10:29:05 -0500

    Actualizacion

--- NuUkAppMobile/app/src/main/java/com/example/nuuk/nuukappmobile/Sec_location.java
+++ NuUkAppMobile/app/src/main/java/com/example/nuuk/nuukappmobile/Sec_location.java
@@ -5,14 +5,21 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 /**
  * Created by Kody on 28/03/2015.
  */
 public class Sec_location extends Fragment {
+    private Spinner spinType;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View rootView = inflater.inflate(R.layout.lay_location, container, false);
         return rootView;
+    }
+    public Sec_location()
+    {
+        spinType = (Spinner) getView().findViewById(R.id.spin_type);
     }
 }
