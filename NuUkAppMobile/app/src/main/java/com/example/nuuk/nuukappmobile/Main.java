d86aeaa7bfb618bc9671c4ab971409b968c21b0b
Alejandro <alejandro_maf@hotmail.com>
2015-04-08 08:20:28 -0500

    Test
    Avanze día 1

--- NuUkAppMobile/app/src/main/java/com/example/nuuk/nuukappmobile/Main.java
+++ NuUkAppMobile/app/src/main/java/com/example/nuuk/nuukappmobile/Main.java
@@ -182,5 +182,4 @@ public class Main extends ActionBarActivity {
         // Cambiar las configuraciones del drawer si hubo modificaciones
         drawerToggle.onConfigurationChanged(newConfig);
     }
-
 }
--- NuUkAppMobile/app/src/main/java/com/example/nuuk/nuukappmobile/Sec_test.java
+++ NuUkAppMobile/app/src/main/java/com/example/nuuk/nuukappmobile/Sec_test.java
@@ -5,31 +5,93 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.ToggleButton;
+
+import com.example.nuuk.nuukappmobile.NuukClass.ColumnsTables;
+import com.example.nuuk.nuukappmobile.SQLite.Querys;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Kody on 28/03/2015.
  */
 public class Sec_test extends Fragment {
     ToggleButton tb;
+    TextView question;
+    View rootView;
+    Querys querys;
+    ColumnsTables ct;
+    ImageView next;
+    int tableSize, currentPosition = 1;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        View rootView = inflater.inflate(R.layout.lay_test, container, false);
+        rootView = inflater.inflate(R.layout.lay_test, container, false);
+        next = (ImageView)rootView.findViewById(R.id.nextbutton);
+        next.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                generateQuestionSection();
+            }
+        });
         tb=(ToggleButton)rootView.findViewById(R.id.tb1);
+        tb.setText("");
         tb.setTextOn("SI");
         tb.setTextOff("NO");
         tb=(ToggleButton)rootView.findViewById(R.id.tb2);
+        tb.setText("");
         tb.setTextOn("SI");
         tb.setTextOff("NO");
         tb=(ToggleButton)rootView.findViewById(R.id.tb3);
+        tb.setText("");
         tb.setTextOn("SI");
         tb.setTextOff("NO");
         tb=(ToggleButton)rootView.findViewById(R.id.tb4);
+        tb.setText("");
         tb.setTextOn("SI");
         tb.setTextOff("NO");
         tb=(ToggleButton)rootView.findViewById(R.id.tb5);
+        tb.setText("");
         tb.setTextOn("SI");
         tb.setTextOff("NO");
+        querys = new Querys(rootView.getContext(),"encuesta");
+        ct = new ColumnsTables();
+        querys.listado(ct.getTableEncuesta(),1);
+        setCurrentSize(querys.lista.size());
+        generateQuestionSection();
+
         return rootView;
     }
+    /*----------------Local Function for test-----------------------------------------*/
+    public void generateQuestionSection(){
+        int size = getCurrentSize();
+        if(size>5) {
+            question = (TextView) rootView.findViewById(R.id.preg1);
+            question.setText(querys.lista.get(currentPosition));
+            question = (TextView) rootView.findViewById(R.id.preg2);
+            question.setText(querys.lista.get(currentPosition+1));
+            question = (TextView) rootView.findViewById(R.id.preg3);
+            question.setText(querys.lista.get(currentPosition+2));
+            question = (TextView) rootView.findViewById(R.id.preg4);
+            question.setText(querys.lista.get(currentPosition+3));
+            question = (TextView) rootView.findViewById(R.id.preg5);
+            question.setText(querys.lista.get(currentPosition+4));
+            currentPosition += 5;
+            setCurrentSize(getCurrentSize()-5);
+        }
+    }
+    /*-------------get & set values for table size---------------------------*/
+    public int getCurrentSize() {
+        return tableSize;
+    }
+    public void setCurrentSize(int size) {
+        this.tableSize = size;
+    }
+
 }
